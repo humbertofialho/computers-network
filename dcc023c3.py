dcae14026b0b96267bec6376ed609c0e835159c9
humbertofialho <humbertofialho@gmail.com>
2018-04-01 22:31:24 -0300

    especificação e primeiras linhas de código

---
+++ dcc023c3.py
@@ -0,0 +1,43 @@
+# --------------------------------------------------------------------------- #
+# ---------------------- Engenharia de Sistemas - UFMG ---------------------- #
+# ----------------------- DCC023 - Redes de Computadores -------------------- # 
+# --------------------- Prof. Ítalo Fernando Scota Cunha -------------------- #
+# ------------------------ Trabalho Prático I - DCCNET ---------------------- #
+# ----------- Alunos :   Humberto Monteiro Fialho   (2013430811) ------------ #
+# -----------            Rafael Carneiro de Castro  (2013XXXXXX) ------------ #
+# --------------------------------------------------------------------------- #
+
+# codification
+def encrypt(number):
+     binary = bin(number);
+     binary = binary[2:len(binary)]
+     binaryInt = int(binary)
+return binaryInt;
+
+def decrypt(number):
+     decimal = dec(number);
+     decimal = decimal[2:len(decimal)]
+     decimalInt = int(decimal)
+return decimalInt;
+
+# framework
+def framework ():
+    
+return 0;
+
+# error detection
+def errorDetection ():
+    
+return 0;
+
+# sequencing
+def sequencing():
+    
+return 0;
+
+#  retransmission
+def retransmission ():
+    
+return 0;
+
+# main
