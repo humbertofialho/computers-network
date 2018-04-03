4e27f22825f40a76844982ea24cf2661e548796d
humbertofialho <humbertofialho@gmail.com>
2018-04-03 18:23:06 -0300

    Add files via upload
    inicializando sockets

--- dcc023c3.py
+++ dcc023c3.py
@@ -7,15 +7,18 @@
 # -----------            Rafael Carneiro de Castro  (2013XXXXXX) ------------ #
 # --------------------------------------------------------------------------- #
 
+# librarys
+from socket import *
+
 # codification
 def encrypt(number):
-     binary = bin(number);
+     binary = bin(number)
      binary = binary[2:len(binary)]
      binaryInt = int(binary)
 return binaryInt;
 
 def decrypt(number):
-     decimal = dec(number);
+     decimal = dec(number)
      decimal = decimal[2:len(decimal)]
      decimalInt = int(decimal)
 return decimalInt;
@@ -41,3 +44,25 @@ def retransmission ():
 return 0;
 
 # main
+def main ():
+    
+    # reading inputs
+    ip = 
+    port = 
+    Input = 
+    output = 
+    
+    
+    # starting socket with standard protocol = 0
+    s = socket(AF_INET, SOCK_STREAM)
+    # server and port
+    s.bind(ip, port)
+    
+    
+return;
+    
+
+# --------------------------------------------------------------------------- #
+
+if __name__ == '__main__': # calling main function
+    main()
