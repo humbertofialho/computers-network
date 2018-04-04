efd32df021503c6193389af5c2f6947607dfd0d0
Rafael Carneiro de Castro <rafaelcarneiroget@hotmail.com>
2018-04-04 18:17:04 -0300

    Operating as server and client

--- dcc023c3.py
+++ dcc023c3.py
@@ -8,7 +8,63 @@
 # --------------------------------------------------------------------------- #
 
 # librarys
+import sys
 import socket as sck
+
+
+# start the code as server
+def initialize_server():
+    # reading inputs
+    ip = '127.0.0.1'
+    port = int(sys.argv[2])
+    input_file_name = sys.argv[3]
+    output_file_name = sys.argv[4]
+
+    # starting socket with standard protocol
+    s = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
+    s.setsockopt(sck.SOL_SOCKET, sck.SO_REUSEADDR, 1)
+    s.bind((ip, port))
+    s.listen(1)
+
+    print('Server running on port', port)
+    connection = s.accept()[0]
+
+    print('Esperando dados:')
+    data1 = connection.recv(4)
+    print(data1)
+    print(type(data1))
+
+    print('Esperando dados2:')
+    data2 = connection.recv(4)
+    print(data2)
+
+    return
+
+
+# start the code as client
+def initialize_client():
+    # reading inputs
+    ip = sys.argv[2].split(':')[0]
+    port = int(sys.argv[2].split(':')[1])
+    input_file_name = sys.argv[3]
+    output_file_name = sys.argv[4]
+
+    s = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
+    s.setsockopt(sck.SOL_SOCKET, sck.SO_REUSEADDR, 1)
+    s.connect((ip, port))
+    print('Mandando')
+    resp = s.send('abcdefgh'.encode())
+    print('RESPOSTA:')
+    print(resp)
+
+
+    # print('Mandando2')
+    # resp = s.send('efgh'.encode())
+    # print('RESPOSTA2:')
+    # print(resp)
+
+    s.close()
+    print('Done')
 
 
 # codification
@@ -82,27 +138,10 @@ def receive():
 
 # main
 def main():
-    # reading inputs
-    ip = '127.0.0.1'
-    port = 5050
-    # input =
-    # output =
-
-
-    # starting socket with standard protocol = 0
-    s = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-    # server and port
-    s.bind((ip, port))
-    s.listen(1)
-    print('Servidor na porta 5050')
-    connection = s.accept()[0]
-
-    print('Esperando dados:')
-    data1 = connection.recv(8)
-    print(data1)
-    print(type(data1))
-    # data2 = connection.recv(4)
-    # print(data2)
+    if sys.argv[1] == '-s':
+        initialize_server()
+    elif sys.argv[1] == '-c':
+        initialize_client()
 
     return
 
