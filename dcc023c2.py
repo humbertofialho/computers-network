db88ffcf2a9f24f87c64f3fda9793b0f8faac723
Rafael Carneiro de Castro <rafaelcarneiroget@hotmail.com>
2018-04-19 20:19:50 -0300

    Adjusting unispected data

--- dcc023c2.py
+++ dcc023c2.py
@@ -18,6 +18,7 @@ import socket as sck
 MAX_LENGTH = 65000
 SYNC = 'dcc023c2'
 
+
 class DataTransfer:
     def __init__(self, data='', id=1, flags=0):
         self.data = data
@@ -35,12 +36,14 @@ class DataTransfer:
         return pattern.format(number)
 
     def checksum(self):
-        sum = 0
-		for i in range(len(string)):
-			sum = sum + ord(string[i])
-		temp = sum % 64  	# mod64 - can change to other length
-		rem = -temp  		# two's complement, easier way
-		return '%2X' % (rem & 0xFF)
+        # TODO implementar
+        return 0
+        # sum = 0
+        # for i in range(len(string)):
+        #     sum = sum + ord(string[i])
+        # temp = sum % 64  	# mod64 - can change to other length
+        # rem = -temp  		# two's complement, easier way
+        # return '%2X' % (rem & 0xFF)
 
     # encrypt
     def encode16(self):
@@ -58,6 +61,7 @@ class DataTransfer:
         encoded_data = self.encode16()
         header = (formatted_length + formatted_checksum + formatted_id + formatted_flags).encode()
         return header + encoded_data
+
 
 # global variables
 data_to_send = DataTransfer(id=0)
@@ -84,8 +88,8 @@ def initialize_server():
 
     print(datetime.datetime.now(), 'Server running on port', port)
     connection = s.accept()[0]
-	
-	# starting one thread to send and other to receive
+
+    # starting one thread to send and other to receive
     send_thread = threading.Thread(target=send_data, args=(connection, input_file_name))
     send_thread.start()
     receive_thread = threading.Thread(target=receive_data, args=(connection, output_file_name))
@@ -100,7 +104,7 @@ def initialize_client():
     input_file_name = sys.argv[3]
     output_file_name = sys.argv[4]
 
-	# deleting output file before to use, if exist
+    # deleting output file before to use, if exist
     if os.path.exists(output_file_name):
         os.remove(output_file_name)
 
@@ -108,7 +112,7 @@ def initialize_client():
     connection.setsockopt(sck.SOL_SOCKET, sck.SO_REUSEADDR, 1)
     connection.connect((ip, port))
 
-	# starting one thread to send and other to receive
+    # starting one thread to send and other to receive
     send_thread = threading.Thread(target=send_data, args=(connection, input_file_name))
     send_thread.start()
     receive_thread = threading.Thread(target=receive_data, args=(connection, output_file_name))
@@ -135,7 +139,7 @@ def send_data(connection, file_name):
                 global ack_timeout
                 ack_timeout = True
 
-			# to measure time 
+            # to measure time
             timer = threading.Timer(1, handle_timeout)
             timer.start()
             while True:
@@ -148,14 +152,15 @@ def send_data(connection, file_name):
                     data_to_send.prepare_for_new_data()
                     break
 
+
 # function to receive data from file
 def receive_data(connection, file_name):
     last_received_id = 1
 
     while True:
         print(datetime.datetime.now(), 'Waiting for data.')
-		
-		# finding SYNC expression
+
+        # finding SYNC expression
         sync = connection.recv(8)
         if sync.decode() != SYNC:
             print(datetime.datetime.now(), 'Resynchronizing...')
@@ -166,7 +171,7 @@ def receive_data(connection, file_name):
             print(datetime.datetime.now(), 'Resynchronizing...')
             continue
 
-		# collecting informations in the string
+        # collecting information in the string
         length = connection.recv(4)
         data_to_receive.length = int(length.decode(), base=16)
 
@@ -179,33 +184,40 @@ def receive_data(connection, file_name):
         flags = connection.recv(2)
         data_to_receive.flags = int(flags.decode(), base=16)
 
-        data = connection.recv(2*data_to_receive.length)
-        data_to_receive.decode16(data)
-		
-		# verifying checksum value
+        try:
+            data = connection.recv(2*data_to_receive.length)
+            data_to_receive.decode16(data)
+        except binascii.Error or UnicodeDecodeError:
+            print(datetime.datetime.now(), 'Conversion error!')
+            continue
+
+        # verifying checksum value
         if data_to_receive.checksum() != received_checksum:
             print(datetime.datetime.now(), 'Checksum error!')
             continue
 
         if data_to_receive.flags == 128:
-            # tratar ACK recebido
+            # treat received ACK
             if data_to_receive.id == data_to_send.id:
                 print(datetime.datetime.now(), 'ACK received.')
                 data_to_send.confirmed = True
-			else: # isso ??
-				print(datetime.datetime.now(), 'ACK not received: different id.')
-                data_to_send.confirmed = False
-			
+
         else:
-            # tratar novo dado
+            # treat new data
             expected_id = 1 if last_received_id == 0 else 0
             if data_to_receive.id != expected_id:
-                print(datetime.datetime.now(), 'Unexpected data.')
+                print(datetime.datetime.now(), 'Retransmission data, resending ACK.')
+                data_to_receive.data = ''
+                data_to_receive.flags = 128
+
+                connection.send(SYNC.encode())
+                connection.send(SYNC.encode())
+                connection.send(data_to_receive.get_frame())
                 continue
 
             with open(file_name, 'a') as file:
                 file.write(data_to_receive.data)
-                print(datetime.datetime.now(), 'Data write on file', file_name)
+                print(datetime.datetime.now(), 'Data written on file {}, sending ACK.'.format(file_name))
                 data_to_receive.data = ''
                 data_to_receive.flags = 128
                 last_received_id = data_to_receive.id
@@ -213,6 +225,7 @@ def receive_data(connection, file_name):
                 connection.send(SYNC.encode())
                 connection.send(SYNC.encode())
                 connection.send(data_to_receive.get_frame())
+
 
 # main: calling functions to receive inputs
 def main():
@@ -222,6 +235,7 @@ def main():
         initialize_client()
 
     return
+
 
 # --------------------------------------------------------------------------- #
 # calling main function
