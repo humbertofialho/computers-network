883d3ea2e11531849d36c28472ff837a7401a5d6
Rafael Carneiro de Castro <rafaelcarneiroget@hotmail.com>
2018-05-21 22:31:20 -0300

    Comments for implementation

--- tp2/router.py
+++ tp2/router.py
@@ -6,3 +6,6 @@
 # ----------- Alunos :   Humberto Monteiro Fialho   (2013430811) ------------ #
 # --------------------   Rafael Carneiro de Castro  (2013030210) ------------ #
 # --------------------------------------------------------------------------- #
+
+# manter um histórico com as rotas não otimizadas para fazer troca instantânea
+# quando um enlace for desativado
