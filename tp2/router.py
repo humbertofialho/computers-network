1238b0f42617c5ea3c50a0336840daf023217d49
Rafael Carneiro de Castro <rafaelcarneiroget@hotmail.com>
2018-05-25 17:07:21 -0300

    Threading tests

--- tp2/router.py
+++ tp2/router.py
@@ -16,6 +16,9 @@
 
 import sys
 import json
+# TODO remove import
+import time
+import threading
 import socket as sck
 
 IP = '127.0.1.1'
@@ -31,16 +34,28 @@ socket.sendto('teste'.encode(), (IP, PORT))
 socket.sendto(string.encode(), (IP, PORT))
 
 
-def star_router():
-    pass
+def star_router(address, update_period, startup_commands):
+    # TODO remove debug print
+    print(address, update_period, startup_commands)
+
+    # TODO remove tests
+    read_thread = threading.Thread(target=read_commands, args=())
+    read_thread.start()
+    print_thread = threading.Thread(target=read_command_file, args=())
+    print_thread.start()
 
 
 def read_command_file():
-    pass
+    # TODO remove tests
+    for index in range(5):
+        print('File processed', index)
+        time.sleep(1)
 
 
 def read_commands():
-    pass
+    # TODO remove tests
+    read = input()
+    print('Read data>', read)
 
 
 # main: calling functions to receive inputs
@@ -62,7 +77,7 @@ def main():
         if '--startup-commands' in sys.argv:
             startup_commands = sys.argv[sys.argv.index('--startup-commands') + 1]
 
-    print(address, update_period, startup_commands)
+    star_router(address, update_period, startup_commands)
 
     return
 
