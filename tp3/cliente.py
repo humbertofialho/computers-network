3112eae94e25085cc20af6a7ba5fab8b08173f18
Rafael Carneiro de Castro <rafaelcarneiroget@hotmail.com>
2018-07-04 17:50:29 -0300

    Main logic

--- tp3/cliente.py
+++ tp3/cliente.py
@@ -19,25 +19,23 @@ def main():
         server_ip = sys.argv[2]
         server_port = int(sys.argv[3])
 
-        # TODO remover
-        print(local_port, server_port, server_ip)
-
         socket = sck.socket(sck.AF_INET, sck.SOCK_DGRAM)
         socket.setsockopt(sck.SOL_SOCKET, sck.SO_REUSEADDR, 1)
         socket.bind(('127.0.0.1', local_port))
 
         while True:
+            # select incoming from terminal and socket
             reads, _, _ = select.select([sys.stdin, socket], [], [])
             for read in reads:
-                # message from server
                 if read == socket:
-                    data = read.recv(1024)
+                    # message from server
+                    data = read.recv(500)
                     if data:
-                        # prints the message received
-                        print('New message: ' + data.decode())
+                        # print the message received
+                        print(data.decode())
 
-                # message from terminal
                 else:
+                    # message from terminal, send command to server
                     message = sys.stdin.readline()
                     socket.sendto(message.encode(), (server_ip, server_port))
 
--- tp3/servidor.py
+++ tp3/servidor.py
@@ -16,6 +16,34 @@ class Server:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
+        self.tag_map = dict()
+
+    def add_to_tag(self, tag, address):
+        # add new listeners for tag in the map
+        if tag in self.tag_map:
+            self.tag_map[tag].add(address)
+        else:
+            self.tag_map[tag] = {address}
+
+    def remove_from_tag(self, tag, address):
+        # remove listener from tag
+        if tag in self.tag_map and address in self.tag_map[tag]:
+            self.tag_map[tag].remove(address)
+
+    def send_message(self, message, socket):
+        # find all tags in the message
+        tags = list(set(re.findall('(?:#)(\w+)', message)))
+
+        # set to avoid double dispatch
+        to_send = set()
+        for tag in tags:
+            if tag in self.tag_map:
+                registered = self.tag_map[tag]
+                to_send = to_send.union(registered)
+
+        for address in to_send:
+            # send the message for each registered listener
+            socket.sendto(message.encode(), address)
 
 
 server = None
@@ -32,22 +60,26 @@ def start_server(ip, port):
     while True:
         message, address = socket.recvfrom(500)
         message = message.decode()
-        tags = list(set(re.findall('(?:#)(\w+)', message)))
 
-        # TODO remover
-        print('RECEIVED MESSAGE>', message)
-        print(tags)
-        print('Address:', address)
-        socket.sendto('Teste do servidor para o cliente'.encode(), address)
+        # check if it is a message do add or remove tag
+        to_add = list(set(re.findall('(?:\+)(\w+)', message)))
+        to_remove = list(set(re.findall('(?:-)(\w+)', message)))
+        if len(to_add) > 0:
+            for tag in to_add:
+                server.add_to_tag(tag, address)
+            socket.sendto('Tag adicionada com sucesso.'.encode(), address)
+        elif len(to_remove) > 0:
+            for tag in to_remove:
+                server.remove_from_tag(tag, address)
+            socket.sendto('Tag removida com sucesso.'.encode(), address)
+        else:
+            server.send_message(message, socket)
 
 
 # main: calling functions to receive inputs
 def main():
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
-
-        # TODO remover
-        print(port)
         start_server('127.0.0.1', port)
 
     return
